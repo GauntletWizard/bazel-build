9ba05e78f4225df07675027c77b787e0a0abe180
Thomas Hahn <Thahn@tcbtech.com>
2019-04-02 19:57:24 -0700

    Not in china

--- pre_install.sh
+++ pre_install.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# change sources to mirror in China
-mv /etc/apt/sources.list /etc/apt/sources.list.save
-mv sources.list /etc/apt/sources.list
-
 # set timezone
 apt-get update
 apt-get install -y tzdata
-echo Asia/Shanghai > /etc/timezone && dpkg-reconfigure --frontend noninteractive tzdata
